cf57b0793d857d5813af1632435c99437fc63de2
s18915JW <s18915@pjwstk.edu.pl>
2020-03-17 09:55:36 +0100

    controller (3)

---
+++ FirstAPI/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Jankowski, Maciejewski, Andrzejewski";
+        }
+
+    }
+}
